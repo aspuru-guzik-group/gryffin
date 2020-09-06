8ad8faccff14bd4c6a2bf1be27b6bc58874076a6
Kobi Felton <kobi.c.f@gmail.com>
2020-09-06 16:55:09 +0100

    Update setup.py and .gitignore

--- setup.py
+++ setup.py
@@ -1,10 +1,11 @@
 #!/usr/bin/env python 
 
-import versioneer
-import numpy as np
+# import versioneer
+# import numpy as np
 
 from setuptools import setup, find_packages
 from distutils.extension import Extension
+from distutils.command.build import build as build_orig
 
 #===============================================================================
 
@@ -14,39 +15,31 @@ def readme():
 
 #===============================================================================
 
-try: 
-	from Cython.Distutils import build_ext
-	from Cython.Build     import cythonize
-except ImportError:
-	use_cython = False
-else:
-	use_cython = True
+ext_modules = [
+	Extension('gryffin.bayesian_network.kernel_evaluations',
+		['src/gryffin/bayesian_network/kernel_evaluations.c']),
+	Extension('gryffin.bayesian_network.kernel_prob_reshaping',
+		['src/gryffin/bayesian_network/kernel_prob_reshaping.c']),]
 
-cmdclass    = {}
-ext_modules = []
-use_cython = False
 
-if use_cython:
-	ext_modules += [
-		Extension('gryffin.bayesian_network.kernel_evaluations',
-			['src/gryffin/bayesian_network/kernel_evaluations.pyx']),
-		Extension('gryffin.bayesian_network.kernel_prob_reshaping',
-			['src/gryffin/bayesian_network/kernel_prob_reshaping.pyx']),]
-	ext_modules = cythonize(ext_modules)
-	cmdclass.update({'build_ext': build_ext})
-else:
-	ext_modules += [
-		Extension('gryffin.bayesian_network.kernel_evaluations',
-			['src/gryffin/bayesian_network/kernel_evaluations.c']),
-		Extension('gryffin.bayesian_network.kernel_prob_reshaping',
-			['src/gryffin/bayesian_network/kernel_prob_reshaping.c']),]
+class build(build_orig):
+
+    def finalize_options(self):
+        super().finalize_options()
+        # I stole this line from ead's answer:
+        __builtins__.__NUMPY_SETUP__ = False
+        import numpy
+        # or just modify my_c_lib_ext directly here, ext_modules should contain a reference anyway
+        extension = next(m for m in self.distribution.ext_modules if m == ext_modules[0])
+        extension.include_dirs.append(numpy.get_include())
+
 
 #===============================================================================
 
 setup(name='gryffin',
 	#version=versioneer.get_version(),
 	version='0.1.0',
-        cmdclass=versioneer.get_cmdclass(),
+    # cmdclass=versioneer.get_cmdclass(),
 	description='Bayesian optimization for categorical variables', 
 	long_description=readme(),
 	long_description_content_type='text/markdown',
@@ -62,11 +55,11 @@ setup(name='gryffin',
 	package_dir={'': 'src'},
 	zip_safe=False,
 	ext_modules=ext_modules,
+	cmdclass={'build': build},
 	tests_require=['pytest'],
 	install_requires=[
 		'numpy',
 		'sqlalchemy',
 		],
-	include_dirs=np.get_include(),
 	python_requires='>=3.6',
 )
