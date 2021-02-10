d3eced0b9103cabcf1ee2e1baaeee56f648dcb9b
Matteo Aldeghi <maldegh@mpibpc.mpg.de>
2021-02-10 18:06:36 -0500

    use external chimera package

--- src/gryffin/utilities/defaults.py
+++ src/gryffin/utilities/defaults.py
@@ -2,61 +2,61 @@
 
 __author__ = 'Florian Hase'
 
-#=======================================================================
-# DEFAULT GENERAL CONFIGURATIONS
-
+# =============================
+# Default general configuration
+# =============================
 default_general_configurations = {
-	'auto_desc_gen':          False,
-	'backend':               'tensorflow',
-	'batches':                1,
-	'boosted':                True,
-	'num_cpus':               1,  # Options are a number, or 'all'
-	'random_seed':            100691,
-	'sampler':               'uniform',
-	'sampling_strategies':    2,
-	'save_database':          True,
-	'scratch_dir':           './.scratch',
-	'softness':               0.001,
-	'feas_sensitivity':       1,
-	'continuous_optimizer':  'adam',
-	'categorical_optimizer': 'naive',
-	'discrete_optimizer':    'naive',
-	'verbosity': {
-			'default':          2,
-			'bayesian_network': 3,
-			'random_sampler':   2,
-		}
+    'backend':               'tensorflow',  # Options are 'tensorflow' or 'edward'
+    'num_cpus':               1,  # Options are a number, or 'all'
+    'boosted':                True,
+    'auto_desc_gen':          False,
+    'batches':                1,
+    'sampling_strategies':    2,
+    'softness':               0.001,  # softness of Chimera for multiobj optimizations
+    'feas_sensitivity':       1,  # sensitivity to feasibility constraints
+    'random_seed':            100691,
+    'sampler':               'uniform',
+    'save_database':          True,
+    'scratch_dir':           './.scratch',
+    'continuous_optimizer':  'adam',
+    'categorical_optimizer': 'naive',
+    'discrete_optimizer':    'naive',
+    'verbosity': {
+        'default':          2,
+        'bayesian_network': 3,
+        'random_sampler':   2,
+    }
 }
 
-#=======================================================================
-# DEFAULT DATABASE CONFIGURATIONS
 
+# ==============================
+# Default database configuration
+# ==============================
 default_database_configurations = {
-	'format':           'sqlite',
-	'path':             './SearchProgress',
-	'log_observations':  True,
-	'log_runtimes':      True,
+    'format':           'sqlite',
+    'path':             './SearchProgress',
+    'log_observations':  True,
+    'log_runtimes':      True,
 }
 
-#=======================================================================
-
+# =============================
+# Default overall configuration
+# =============================
 default_configuration = {
-	'general': {
-			key: default_general_configurations[key] for key in default_general_configurations.keys()
-		},
+    'general': {
+        key: default_general_configurations[key] for key in default_general_configurations.keys()
+    },
     'database': {
-			key: default_database_configurations[key] for key in default_database_configurations.keys()
-		},
-	'parameters': [
-			{'name': 'param_0', 'type': 'continuous', 'low':  0.0, 'high': 10.0, 'size': 2},
-			{'name': 'param_1', 'type': 'continuous', 'low': -1.0, 'high':  1.0, 'size': 1},
-		],
-	'objectives': [ 
-			{'name': 'obj_0', 'goal': 'minimize', 'hierarchy': 0, 'tolerance': 0.2},
-			{'name': 'obj_1', 'goal': 'maximize', 'hierarchy': 1, 'tolerance': 0.2},
-		]
+        key: default_database_configurations[key] for key in default_database_configurations.keys()
+    },
+    'parameters': [
+        {'name': 'param_0', 'type': 'continuous', 'low': 0, 'high': 1, 'size': 1},
+        {'name': 'param_1', 'type': 'continuous', 'low': 0, 'high': 1, 'size': 1},
+    ],
+    'objectives': [
+        {'name': 'obj_0', 'goal': 'min', 'tolerance': 0.2, 'absolute': False},
+        {'name': 'obj_1', 'goal': 'max', 'tolerance': 0.2, 'absolute': False},
+    ]
 }
-
-#=======================================================================
 
 
