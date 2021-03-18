0d326f4507a68b993ab3b8e2eaf08189a5409111
Matteo Aldeghi <maldegh@mpibpc.mpg.de>
2021-03-18 11:22:25 -0400

    use base 10

--- src/gryffin/utilities/infos.py
+++ src/gryffin/utilities/infos.py
@@ -19,11 +19,11 @@ def parse_time(start, end):
 
 def memory_usage():
     if sys.platform == 'darwin':  # MacOS --> memory in bytes
-        kB = resource.getrusage(resource.RUSAGE_SELF).ru_maxrss // 1024
+        kB = resource.getrusage(resource.RUSAGE_SELF).ru_maxrss // 1000.
     else:  # Linux --> memory in kilobytes
         kB = resource.getrusage(resource.RUSAGE_SELF).ru_maxrss
 
-    GB = kB // 1048576
-    MB = kB // 1024
-    kB = kB % 1024
+    GB = kB // 1000000.
+    MB = kB // 1000.
+    kB = kB % 1000.
     return GB, MB, kB
