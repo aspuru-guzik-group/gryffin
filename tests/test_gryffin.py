2f957dd413597625ea4c9cfc56a2f35a144a6ee8
Matteo Aldeghi <maldegh@mpibpc.mpg.de>
2021-03-30 12:18:45 -0400

    update tests

--- tests/test_gryffin.py
+++ tests/test_gryffin.py
@@ -8,52 +8,45 @@ def test_recommend():
     config = {
         "general": {
             "save_database": False,
-            "scratch_dir": 'tests_scratch',
             "num_cpus": 1,
             "boosted": True,
             "sampling_strategies": 2,
             "random_seed": 42,
         },
-        "parameters": [{"name": "param_0", "type": "continuous", "low": 0, "high": 1, "size": 1},
-                       {"name": "param_1", "type": "continuous", "low": 0, "high": 1, "size": 1}],
+        "parameters": [{"name": "param_0", "type": "continuous", "low": 0, "high": 1},
+                       {"name": "param_1", "type": "continuous", "low": 0, "high": 1}],
         "objectives": [{"name": "obj", "goal": "min"}]
         }
 
     observations = [
-        {'param_0': np.array([0.3]), 'param_1': np.array([0.4]), 'obj': 0.1},
-        {'param_0': np.array([0.5]), 'param_1': np.array([0.6]), 'obj': 0.2},
+        {'param_0': 0.3, 'param_1': 0.4, 'obj': 0.1},
+        {'param_0': 0.5, 'param_1': 0.6, 'obj': 0.2},
     ]
 
     gryffin = Gryffin(config_dict=config)
     _ = gryffin.recommend(observations=observations)
-
-    # cleanup folders
-    shutil.rmtree('tests_scratch')
 
 
 def test_multiobjective():
     config = {
         "general": {
             "save_database": False,
-            "scratch_dir": 'tests_scratch',
             "num_cpus": 1,
             "boosted": True,
             "sampling_strategies": 2,
             "random_seed": 42,
         },
-        "parameters": [{"name": "param_0", "type": "continuous", "low": 0, "high": 1, "size": 1},
-                       {"name": "param_1", "type": "continuous", "low": 0, "high": 1, "size": 1}],
+        "parameters": [{"name": "param_0", "type": "continuous", "low": 0, "high": 1},
+                       {"name": "param_1", "type": "continuous", "low": 0, "high": 1}],
         "objectives": [{"name": "obj0", "goal": "min", "tolerance": 0.2, "absolute": False},
                        {"name": "obj1", "goal": "max", "tolerance": 0.1, "absolute": False}]
     }
 
     observations = [
-        {'param_0': np.array([0.3]), 'param_1': np.array([0.4]), 'obj0': 0.1, 'obj1': 0.2},
-        {'param_0': np.array([0.5]), 'param_1': np.array([0.6]), 'obj0': 0.2, 'obj1': 0.1},
+        {'param_0': 0.3, 'param_1': 0.4, 'obj0': 0.1, 'obj1': 0.2},
+        {'param_0': 0.5, 'param_1': 0.6, 'obj0': 0.2, 'obj1': 0.1},
     ]
 
     gryffin = Gryffin(config_dict=config)
     _ = gryffin.recommend(observations=observations)
 
-    # cleanup folders
-    shutil.rmtree('tests_scratch')
