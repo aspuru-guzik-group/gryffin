ae70c06e97a1a5a24dd35848f7f466fbec26b461
Matteo Aldeghi <maldegh@mpibpc.mpg.de>
2021-04-07 11:35:48 -0400

    update cli

--- src/gryffin/cli.py
+++ src/gryffin/cli.py
@@ -6,6 +6,8 @@ import os
 import shutil
 import json
 import argparse
+from rich.console import Console
+from rich.table import Table
 from gryffin import Gryffin
 
 
@@ -24,19 +26,57 @@ def parse_options():
     parser.add_argument('-c',
                         dest='json',
                         type=str,
-                        help='Json configuration file.',
+                        help='Json configuration file with parameters and objectives.',
                         required=True)
-    parser.add_argument('--num_experiments',
+    parser.add_argument('-n',
+                        metavar='',
                         dest='num_experiments',
                         type=int,
-                        help='Number of experiments requested. Default is 1.',
+                        help='Number of experiments to suggest. Default is 1. '
+                             'Note that Gryffin will alternate between exploration and exploitation.',
                         default=1)
     parser.add_argument('--num_cpus',
+                        metavar='',
                         dest='num_cpus',
                         type=int,
                         help='Number of CPUs to use. Default is 1.',
                         default=1)
+    parser.add_argument('--optimizer',
+                        metavar='',
+                        dest='optimizer',
+                        type=str.lower,
+                        help='Algorithm to use to optimize the acquisition function. Choices are "adam" or "genetic". '
+                             'Default is "adam".',
+                        default='adam',
+                        choices=['adam', 'genetic'])
+    parser.add_argument('--dynamic',
+                        dest='dynamic',
+                        help='Whether to use dynamic Gryffin. Default is False.',
+                        default=False,
+                        action='store_true')
+    parser.add_argument('--feas_approach',
+                        metavar='',
+                        dest='feas_approach',
+                        type=str.lower,
+                        help='Approach to unknown feasibility constraints. Choices are: '
+                             '"fwa" (feasibility-weighted acquisition), '
+                             '"fca" (feasibility-constrained acquisition), '
+                             '"fia" (feasibility-interpolated acquisition). '
+                             'Default is "fia".',
+                        default='fia',
+                        choices=['fwa', 'fca', 'fia'])
+    parser.add_argument('--boosted',
+                        dest='boosted',
+                        help='Whether to use boosting. Default is False.',
+                        default=False,
+                        action='store_true')
+    parser.add_argument('--cached',
+                        dest='cached',
+                        help='Whether to use caching. Default is False.',
+                        default=False,
+                        action='store_true')
     parser.add_argument('--seed',
+                        metavar='',
                         dest='random_seed',
                         type=int,
                         help='Random seed used for initialization. Default is 42.',
@@ -57,10 +97,9 @@ def main(args):
     elif infile_extension in ['xls', 'xlsx']:
         df_in = pd.read_excel(args.file)
 
-    print("    ================")
-    print("    Past Experiments")
-    print("    ================")
-    print(df_in)
+    # show past experiments
+    print()
+    print_df_as_rich_table(df_in, title='Past Experiments')
 
     # load params/objectives
     with open(args.json, 'r') as jsonfile:
@@ -82,11 +121,6 @@ def main(args):
     # ---------------------
     # Run Gryffin recommend
     # ---------------------
-    print()
-    print("    =====================================")
-    print("    Running Experiment Planning Algorithm")
-    print("    =====================================")
-
     if len(df_in) == 0:
         observations = []
         samples = suggest_next_experiments(gryffin, observations, args.num_experiments)
@@ -105,14 +139,12 @@ def main(args):
     df_samples = pd.DataFrame(columns=df_in.columns)
 
     for param_name in param_names:
-        param_values = [sample[param_name][0] for sample in samples]
+        param_values = [sample[param_name] for sample in samples]
         df_samples.loc[:, param_name] = param_values
 
+    # show proposed experiments
+    print_df_as_rich_table(df_samples, title='Proposed Experiments')
     print()
-    print("    ====================")
-    print("    Proposed Experiments")
-    print("    ====================")
-    print(df_samples)
         
     # append df_samples to df_in
     df_out = df_in.append(df_samples, ignore_index=True, sort=False)
@@ -129,24 +161,79 @@ def main(args):
     elif infile_extension in ['xls', 'xlsx']:
         df_out.to_excel(args.file, index=False)
 
+    # print final remakrs
+    console = Console()
+    console.print('Notes:', style='bold red')
+    console.print(f'- The original file [green]{args.file}[/green] has been backed up as [green]backup_{args.file}[/green]')
+    console.print(f'- The proposed experiments have been appended to the file [green]{args.file}[/green]')
+    obj_names_string = ", ".join(obj_names)
+    console.print(f'- Add the results for the proposed experiments under the columns [green]{obj_names_string}[/green]')
+    print()
+
 
 # =========
 # Functions
 # =========
+def infer_batches_and_strategies(num_experiments):
+
+    # if num_experiments <= 2, we use 2 strategies either in parallel or sequence (done in suggest_next_experiments)
+    if num_experiments <= 2:
+        batches = 1
+        sampling_strategies = 2
+    # if 2 < num_experiments <= 5, use as many sampling strategies as num experiments in one batch
+    elif 2 < num_experiments <= 5:
+        batches = 1
+        sampling_strategies = num_experiments
+    # if num_experiments is > 5, figure out a best split
+    elif 2 < num_experiments <= 21:
+        # if multiple of 5
+        if num_experiments % 5 == 0:
+            sampling_strategies = 5
+            batches = num_experiments / 5
+            return batches, sampling_strategies
+        # if multiple of 4
+        if num_experiments % 4 == 0:
+            sampling_strategies = 4
+            batches = num_experiments / 4
+            return batches, sampling_strategies
+        # if multiple of 3
+        if num_experiments % 3 == 0:
+            sampling_strategies = 3
+            batches = num_experiments / 3
+            return batches, sampling_strategies
+        # if multiple of 2
+        if num_experiments % 2 == 0:
+            sampling_strategies = 2
+            batches = num_experiments / 2
+            return batches, sampling_strategies
+        raise ValueError('please do not select a prime number of experiments')
+    else:
+        raise ValueError('you are selecting a large batch of experiments! This is not ideal. Talk to Matteo if needed.')
+
+    return batches, sampling_strategies
+
+
 def init_objects(args, config):
+
+    batches, sampling_strategies = infer_batches_and_strategies(args.num_experiments)
+
+    if args.feas_approach in ['fia', 'fwa']:
+        feas_param = 1
+    elif args.feas_approach == 'fca':
+        feas_param = 0.5
 
     config["general"] = {
                         "num_cpus": args.num_cpus,
-                        "boosted": True,  # <----
-                        "caching": True,  # <----
-                        "batches": args.batches,
-                        "sampling_strategies": args.sampling_strategies,
-                        "auto_desc_gen": False,  # <----
-                        "feas_approach": "fwa",  # <----
-                        "feas_param": 1,  # < -----
+                        "boosted": args.boosted,
+                        "caching": args.cached,
+                        "batches": int(batches),
+                        "sampling_strategies": int(sampling_strategies),
+                        "auto_desc_gen": args.dynamic,
+                        "feas_approach": args.feas_approach,
+                        "feas_param": feas_param,
                         "random_seed": args.random_seed,
                         "save_database": False,
-                        "acquisition_optimizer": "adam",  # < -------
+                        "acquisition_optimizer": args.optimizer,
                         "verbosity": 3
                         }
 
@@ -156,17 +243,18 @@ def init_objects(args, config):
 
 def suggest_next_experiments(gryffin, observations, num_experiments):
 
-    samples = gryffin.recommend(observations=observations)
-
-    # i.e. sampling_strategies * batches == num_experiments
-    if len(samples) == num_experiments:
-        return samples
     # i.e. sequential experiments with sampling_strategies==2, batches==1
-    elif len(samples) > num_experiments:
-        select = len(observations) % len(samples)
-        return [samples[select]]
+    if num_experiments == 1:
+        # select alternating sampling strategy
+        sampling_strategies = [-1, 1]
+        select_idx = len(observations) % 2
+        strategy = sampling_strategies[select_idx]
+        samples = gryffin.recommend(observations=observations, sampling_strategies=[strategy])
+    # i.e. sampling_strategies * batches == num_experiments
     else:
-        raise ValueError()
+        samples = gryffin.recommend(observations=observations)
+
+    return samples
 
 
 def build_observations(df_observations, param_names, obj_names):
@@ -176,12 +264,28 @@ def build_observations(df_observations, param_names, obj_names):
     for obs in df_observations.to_numpy():
         d = {}
         for param_name, value in zip(param_names, obs):
-            d[param_name] = np.array([value])
+            d[param_name] = value
         for obj_name, value in zip(obj_names, obs):
             d[obj_name] = value
         observations.append(d)
 
     return observations
+
+
+def print_df_as_rich_table(df, title):
+    console = Console()
+
+    table = Table(show_header=True, header_style="bold red", title=title)
+    table.add_column("N")
+    for col in df.columns:
+        table.add_column(col)
+
+    np_data = df.to_numpy()
+    for i, row in enumerate(np_data):
+        row_str = [f'{i + 1:d}'] + [f'{x:f}' for x in row]
+        table.add_row(*row_str)
+
+    console.print(table)
 
 
 def entry_point():
