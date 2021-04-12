4ee3cc020efc6b541f702cf61b2db74c7fa2223a
Matteo Aldeghi <maldegh@mpibpc.mpg.de>
2021-04-12 15:52:31 -0400

    refactor adam optimizer

--- src/gryffin/acquisition/gradient_optimizer/adam_optimizer.py
+++ src/gryffin/acquisition/gradient_optimizer/adam_optimizer.py
@@ -1,76 +1,100 @@
 #!/usr/bin/env python 
 
-__author__ = 'Florian Hase'
+__author__ = 'Florian Hase, Matteo Aldeghi'
 
 import numpy as np
 
 
 class AdamOptimizer:
 
-    iterations = 0
-    dx = 1e-6
-
-    def __init__(self, func=None, eta=0.1, beta_1=0.9, beta_2=0.999, epsilon=1e-8, decay=0.0):
+    def __init__(self, func=None, pos=None, eta=0.1, beta_1=0.9, beta_2=0.999, epsilon=1e-8):
         self.func = func
         self.eta = eta
         self.beta_1 = beta_1
         self.beta_2 = beta_2
         self.epsilon = epsilon
-        self.decay = decay
-        self.initial_decay = self.decay
-        self.ms, self.vs = None, None
+        self.iterations = 0
 
-    def _set_func(self, func, pos=None):
-        self.func = func
+        # init Adam parameters if pos is provided
         if pos is not None:
-            self.pos = pos
-            self.num_pos = len(pos)
+            self.init_params(pos)
+        else:
+            self.pos = None
+            self.num_pos = None
+            self.ms = None
+            self.vs = None
+
+        # step used to estimate gradients numerically
+        self.dx = 1e-6
+
+    def init_params(self, pos):
+        """
+        pos : list
+            list of indices corresponding to the continuous variables in the vectors that will be passed to update.
+        """
+        self.pos = np.array(pos)
+        self.num_pos = len(pos)
+        self.ms = np.zeros(self.num_pos)  # moment vector
+        self.vs = np.zeros(self.num_pos)  # exponentially weighted infinity norm
+
+    def reset(self):
+        self.iterations = 0
+        self.ms = np.zeros(self.num_pos)
+        self.vs = np.zeros(self.num_pos)
 
     def set_func(self, func, pos=None):
-        self.iterations = 0
-        self._set_func(func, pos)
-        if hasattr(self, 'ms'):
-            delattr(self, 'ms')
-            delattr(self, 'vs')
+        """
+        func : callable
+            function to be optimized.
+        pos : list
+            list of indices corresponding to the continuous variables in the vectors that will be passed to update.
+        """
+        self.func = func
+        self.reset()
+        if pos is not None:
+            self.init_params(pos)
 
-    def grad(self, sample, step=None):
-        if step is None:
-            step = self.dx
+    def grad(self, sample):
+        """Estimate the gradients"""
         gradients = np.zeros(len(sample), dtype=np.float32)
         perturb = np.zeros(len(sample), dtype=np.float32)
-        for pos_index, pos in enumerate(self.pos):
-            if pos is None:
+
+        for i in self.pos:
+            if i is None:
                 continue
-            perturb[pos] += step
-            gradient = (self.func(sample + perturb) - self.func(sample - perturb)) / (2. * step)
-            gradients[pos] = gradient
-            perturb[pos] -= step
+            perturb[i] += self.dx
+            gradient = (self.func(sample + perturb) - self.func(sample - perturb)) / (2. * self.dx)
+            gradients[i] = gradient
+            perturb[i] -= self.dx
+
         return gradients
 
-    def get_update(self, vector):
-        grads = self.grad(vector)
-        eta = self.eta
-        if self.initial_decay > 0.:
-            eta *= (1. / (1. + self.decay * self.iterations))
-
-        next_iter = self.iterations + 1
-        eta_next = eta * (np.sqrt(1. - np.power(self.beta_2, next_iter)) / (1. - np.power(self.beta_1, next_iter)))
-
-        if not hasattr(self, 'ms'):
-            self.ms = [0. for element in vector]
-            self.vs = [0. for element in vector]
-
-        update = np.zeros(len(vector)).astype(np.float32) + np.nan
-        for index, element, grad, mass, vel in zip(range(len(vector)), vector, grads, self.ms, self.vs):
-            m_next = (self.beta_1 * mass) + (1. - self.beta_1) * grad
-            v_next = (self.beta_2 * vel)  + (1. - self.beta_2) * np.square(grad)
-            p_next = element - eta_next * m_next / (np.sqrt(v_next) + self.epsilon)
-            self.ms[index] = m_next
-            self.vs[index] = v_next
-            update[index]  = p_next
-
+    def get_update(self, sample):
+        """
+        vector : nd.array
+        """
+        # get gradients: g
+        grads = self.grad(sample)
+        # get iteration: t
         self.iterations += 1
-        return np.array(update)
+
+        # eta(t) = eta * sqrt(1 – beta2(t)) / (1 – beta1(t))
+        # where: beta(t) = beta^t
+        eta_next = self.eta * (np.sqrt(1. - np.power(self.beta_2, self.iterations)) /
+                               (1. - np.power(self.beta_1, self.iterations)))
+        # m(t) = beta1 * m(t-1) + (1 – beta1) * g(t)
+        ms_next = (self.beta_1 * self.ms) + (1. - self.beta_1) * grads
+        # v(t) = beta2 * v(t-1) + (1 – beta2) * g(t)^2
+        vs_next = (self.beta_2 * self.vs) + (1. - self.beta_2) * np.square(grads)
+
+        # update sample: x(t) = x(t-1) – eta(t) * m(t) / (sqrt(v(t)) + eps)
+        sample_next = sample - eta_next * ms_next / (np.sqrt(vs_next) + self.epsilon)
+
+        # update params
+        self.ms = ms_next
+        self.vs = vs_next
+
+        return sample_next
 
 
 if __name__ == '__main__':
