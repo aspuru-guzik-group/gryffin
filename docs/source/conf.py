f57743212106374c359464e3f1eeb3d342a78a3a
John Willes <johnwilles@gmail.com>
2022-05-12 12:07:21 -0400

    Add source path to masterdoc

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -26,7 +26,7 @@ release = "0.3.0"
 
 # -- General configuration ---------------------------------------------------
 
-master_doc = 'index'
+master_doc = 'source/index'
 
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named "sphinx.ext.*") or your custom
